c12e3a326812069c8836992fc7554e768aea9efd
ASWINI CHIRUMAMILLA <aswinichirumamilla16@gmail.com>
2025-07-16 16:31:17 +0530

    Create Authentication.jsx

---
+++ src/Pages/Authentication.jsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+import LoginSignUp from "../Components/Authentication/LoginSign/LoginSignUp";
+
+const Authentication = () => {
+  return (
+    <>
+      <LoginSignUp />
+    </>
+  );
+};
+
+export default Authentication;
